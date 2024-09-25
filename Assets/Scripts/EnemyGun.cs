ab1f7c1f01363b68dd04276d9094ed834f925d7d
Colin Spiridonov <colinspiri@gmail.com>
2024-09-24 18:43:48 -0700

    enemies that mvoe and shoot (but bullets dont do anything)

---
+++ Assets/Scripts/EnemyGun.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour {
+    [SerializeField] private float rotationSpeed;
+    
+    [SerializeField] private float firingCooldown;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float bulletSpeed;
+
+    private float _currentFiringCooldown;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtPoint(PlayerMovement.Instance.transform.position);
+
+        if (_currentFiringCooldown <= 0) {
+            FireBullet();
+            _currentFiringCooldown = firingCooldown;
+        }
+        else _currentFiringCooldown -= Time.deltaTime;
+    }
+
+    private void LookAtPoint(Vector3 point)
+    {
+        
+        if (Time.timeScale > 0)
+        {
+            // rotate to look at the point
+            float angle = Mathf.Atan2(point.y - transform.position.y, point.x - transform.position.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+    
+    private void FireBullet() {
+        Bullet bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation).GetComponent<Bullet>();
+        bullet.MoveSpeed = bulletSpeed;
+    }
+}
