1011357c14c030da53e720b311f30425c05dda6f
Colin Spiridonov <colinspiri@gmail.com>
2024-11-01 15:07:32 -0700

    refactored player movement to add acceleration params

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,38 +1,46 @@
 using System;
 using ScriptableObjectArchitecture;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-// This class controls player movement
-// Script is based from Coursera Game Design and Development 1: 2D Shooter project from MSU
 public class PlayerMovement : MonoBehaviour {
     public static PlayerMovement Instance;
     // components
     [SerializeField] private TurnCar turnCar;
-    [Space] 
-    [SerializeField] private float moveSpeed;
     
+    // public constants
+    private enum MovementMode { Linear, Acceleration }
+    [SerializeField] private MovementMode movementMode;
     [SerializeField] private Vector2 speedInnerRadius;
     [SerializeField] private Vector2 speedOuterRadius;
-    [SerializeField] private float anchorPointSpeed;
+    
+    [Header("Linear Movement Mode")]
+    [SerializeField] private float linearSpeed; // 6.5
+
+    [Header("Acceleration Movement Mode")] 
+    [SerializeField] private float startingSpeed;
+    [SerializeField] private float topSpeed;
+    [SerializeField] private float accelerationTime;
+    [SerializeField] private AnimationCurve accelerationCurve;
+    
     [Space]
+    [Header("Heat")]
     [SerializeField] private FloatVariable heat;
     [SerializeField] private float heatIncreaseSpeed;
-
-    private Vector3 _anchorPoint;
+    
+    // state
     
     private void Awake() {
         Instance = this;
     }
 
     private void Start() {
-        _anchorPoint = transform.position;
         heat.Value = 0;
     }
 
     void Update()
     {
         HandleInput();
-        // MoveAnchorPoint();
 
         heat.Value += Time.deltaTime * heatIncreaseSpeed;
         if (heat.Value > 1) heat.Value = 1;
@@ -42,37 +50,47 @@ public class PlayerMovement : MonoBehaviour {
     {
         // move player
         Vector3 movementVector = new Vector3(InputManager.Instance.horizontalMoveAxis, InputManager.Instance.verticalMoveAxis, 0);
-        MovePlayer(movementVector);
+        if (movementMode == MovementMode.Linear) {
+            MovePlayerLinearMode(movementVector);
+        }
+        else if (movementMode == MovementMode.Acceleration) {
+            MovePlayerAccelerationMode(movementVector);
+        }
 
         // set turn direction
         int turnDirection = (movementVector.y == 0) ? 0 : (int)(movementVector.y / Mathf.Abs(movementVector.y));
         turnCar.TurnDirection = turnDirection;
     }
 
-    private void MovePlayer(Vector3 movement) {
+    private void MovePlayerLinearMode(Vector3 movement) {
         if (movement == Vector3.zero) return;
         
-        Vector2 speed = new Vector2(moveSpeed, moveSpeed);
-
-        float distanceValueX = Mathf.InverseLerp(speedInnerRadius.x, speedOuterRadius.x, Mathf.Abs(transform.position.x));
-        float reducedSpeedX = Mathf.Lerp(moveSpeed, 0, distanceValueX);
-        bool movingTowardsCenterX =
-            (transform.position.x < 0 && movement.x > 0) || (transform.position.x > 0 && movement.x < 0);
-        speed.x = movingTowardsCenterX ? moveSpeed : reducedSpeedX;
-        
-        float distanceValueY = Mathf.InverseLerp(speedInnerRadius.y, speedOuterRadius.y, Mathf.Abs(transform.position.y));
-        float reducedSpeedY = Mathf.Lerp(moveSpeed, 0, distanceValueY);
-        bool movingTowardsCenterY =
-            (transform.position.y < 0 && movement.y > 0) || (transform.position.y > 0 && movement.y < 0);
-        speed.y = movingTowardsCenterY ? moveSpeed : reducedSpeedY;
+        Vector2 speed = new Vector2(linearSpeed, linearSpeed);
+        speed = LimitSpeedNearBoundaries(movement, speed);
         
         // Debug.Log("speed = " + speed.x + ", " + speed.y);
 
         transform.Translate(new Vector3(movement.x * speed.x, movement.y * speed.y, 0) * Time.deltaTime);
     }
 
-    private void MoveAnchorPoint() {
-        Vector3 anchorToPlayer = transform.position - _anchorPoint;
-        _anchorPoint += anchorToPlayer * (anchorPointSpeed * Time.deltaTime);
+    private void MovePlayerAccelerationMode(Vector3 movement) {
+        
+    }
+
+    // returns limited speed
+    private Vector2 LimitSpeedNearBoundaries(Vector3 movementDirection, Vector2 speed) {
+        float distanceValueX = Mathf.InverseLerp(speedInnerRadius.x, speedOuterRadius.x, Mathf.Abs(transform.position.x));
+        float reducedSpeedX = Mathf.Lerp(speed.x, 0, distanceValueX);
+        bool movingTowardsCenterX =
+            (transform.position.x < 0 && movementDirection.x > 0) || (transform.position.x > 0 && movementDirection.x < 0);
+        speed.x = movingTowardsCenterX ? speed.x : reducedSpeedX;
+        
+        float distanceValueY = Mathf.InverseLerp(speedInnerRadius.y, speedOuterRadius.y, Mathf.Abs(transform.position.y));
+        float reducedSpeedY = Mathf.Lerp(speed.y, 0, distanceValueY);
+        bool movingTowardsCenterY =
+            (transform.position.y < 0 && movementDirection.y > 0) || (transform.position.y > 0 && movementDirection.y < 0);
+        speed.y = movingTowardsCenterY ? speed.y : reducedSpeedY;
+
+        return speed;
     }
 }
