edec1999b7d59ca61006e00f1c5fa58fdf072f75
jsaqib <83430876+jsaqib@users.noreply.github.com>
2024-11-05 17:14:45 -0800

    heat changes

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,12 +1,20 @@
-﻿using UnityEngine;
+﻿using ScriptableObjectArchitecture;
+using UnityEngine;
+using UnityEngine.Rendering;
 
 public class EnemyHealth : Health {
     [SerializeField] private GameObject explosionEffectPrefab;
     [SerializeField] private AudioClip enemyDie;
+    [SerializeField] private float heatGain;
+    [SerializeField] private FloatVariable heat;
     
+
+
     protected override void DeathEffect() {
         AudioManager.Instance.Play(enemyDie, .6f);
         Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
+        heat.Value += heatGain;
         Destroy(gameObject);
+       
     }
 }
