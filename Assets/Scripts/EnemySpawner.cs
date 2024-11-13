2d0c9f03e1e1b5bcc7d6891e63b5f1ef3f5127e8
Colin Spiridonov <colinspiri@gmail.com>
2024-11-13 13:12:11 -0800

    changed enemy spawning to use enemy types

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
     [Header("Components")]
-    [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private GameObjectCollection enemyCollection;
-    
+    [SerializeField] private GameObject prototypeEnemyPrefab;
+    [SerializeField] private GameObject fighter1Prefab;
+    [SerializeField] private GameObject fighter2Prefab;
+    [SerializeField] private GameObject fighter3Prefab;
+    [SerializeField] private GameObject bomberPrefab;
+    [SerializeField] private GameObject homingMissilePrefab;
+
     [Header("Spawning")]
     [SerializeField] private Vector2 originRange;
     private enum SpawningMode {
@@ -17,8 +22,8 @@ public class EnemySpawner : MonoBehaviour {
     [SerializeField] private SpawningMode spawningMode;
     [SerializeField] private bool spawnRandomAfterLastWave;
 
-    [Header("Wave Spawning")]
-    [SerializeField] private List<int> waves;
+    [Header("Wave Spawning")] 
+    [SerializeField] private WaveSet waveSet;
     [SerializeField] private float timeBetweenWaves;
 
     [Header("Random Spawning")]
@@ -51,7 +56,7 @@ public class EnemySpawner : MonoBehaviour {
     private void UpdateRandomSpawning() {
         if (_randomSpawnTimer > 0) _randomSpawnTimer -= Time.deltaTime;
         else if(enemyCollection.Count < maxEnemyCount) {
-            SpawnEnemy();
+            SpawnEnemy(EnemyType.PrototypeEnemy);
             _randomSpawnTimer = timeBetweenRandomSpawn;
         }
     }
@@ -74,7 +79,7 @@ public class EnemySpawner : MonoBehaviour {
         else if (_waveState == WaveState.DelayWave) {
             if (_waveDelayTimer <= 0) {
                 // if there is no next wave 
-                if (_currentWave + 1 >= waves.Count) {
+                if (_currentWave + 1 >= waveSet.NumWaves) {
                     // either transition to random spawning
                     if (spawnRandomAfterLastWave) {
                         spawningMode = SpawningMode.Random;
@@ -95,20 +100,39 @@ public class EnemySpawner : MonoBehaviour {
     }
     
     private void SpawnWave(int waveIndex) {
-        for (int i = 0; i < waves[waveIndex]; i++) {
-            SpawnEnemy();
+        Wave wave = waveSet.waves[waveIndex];
+
+        foreach (var numEnemies in wave.enemies) {
+            for (int i = 0; i < numEnemies.count; i++) {
+                SpawnEnemy(numEnemies.enemyType);
+            }
         }
     }
 
-    private void SpawnEnemy() {
+    private void SpawnEnemy(EnemyType enemyType) {
+        // get prefab from enemy type
+        var enemyPF = GetEnemyPrefab(enemyType);
+        
         // pick origin position that's onscreen
         var originPosition = new Vector3(Random.Range(-originRange.x, originRange.x), Random.Range(-originRange.y, originRange.y), 0);
         var spawnPosition = Random.onUnitSphere * (originRange.x * 1.5f);
 
         // instantiate prefab at position
-        var enemyMovement = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity).GetComponent<EnemyMovement>();
+        var enemyMovement = Instantiate(enemyPF, spawnPosition, Quaternion.identity).GetComponent<EnemyMovement>();
         enemyMovement.SetAnchor(originPosition);
         
         //Debug.Log("spawned enemy");
+    }
+    
+    private GameObject GetEnemyPrefab(EnemyType type) {
+        return type switch {
+            EnemyType.PrototypeEnemy => prototypeEnemyPrefab,
+            EnemyType.Fighter1 => fighter1Prefab,
+            EnemyType.Fighter2 => fighter2Prefab,
+            EnemyType.Fighter3 => fighter3Prefab,
+            EnemyType.Bomber => bomberPrefab,
+            EnemyType.HomingMissile => homingMissilePrefab,
+            _ => null
+        };
     }
 }
---
+++ Assets/Scripts/Scriptable Objects/WaveSet.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WaveSet", menuName = "Scriptable Objects/WaveSet")]
+public class WaveSet : ScriptableObject {
+    public List<Wave> waves;
+    public int NumWaves => waves.Count;
+}
+
+[Serializable]
+public struct Wave {
+    [Header("Wave Parameters")] 
+    public List<NumEnemies> enemies;
+}
+
+[Serializable]
+public struct NumEnemies {
+    public EnemyType enemyType;
+    public int count;
+}
+
+public enum EnemyType
+{
+    PrototypeEnemy,
+    Fighter1,
+    Fighter2,
+    Fighter3,
+    Bomber,
+    HomingMissile
+}
