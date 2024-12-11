523a797a58d8c9403d9db946987c56a5183791c0
Colin Spiridonov <colinspiri@gmail.com>
2024-12-10 20:47:41 -0800

    enemy movement is smoother, samples from perlin noise

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,41 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ScriptableObjectArchitecture;
+using Random = UnityEngine.Random;
 
 public class EnemyMovement : MonoBehaviour {
+    // components 
     [SerializeField] private GameEvent movedToOrigin;
-    [SerializeField] private float moveSpeed;
+    
+    // constants 
     [SerializeField] private float originMoveSpeed;
-    
-    private enum State { MoveToOrigin, MoveRandomly }
-    private State _currentState;
-    private Vector3 _anchorPosition;
-    
-    // move randomly 
-    [SerializeField] private float randomMoveRadius;
-    [SerializeField] private float changeDirectionTime;
+    [SerializeField] private MinMaxFloat randomMoveSpeed;
+    [SerializeField] private Vector2 randomMoveArea;
     [SerializeField] private Vector2 onscreenSize;
-    [Tooltip("1 when aligned, 0 when opposite")]
-    [Range(0.0f, 1.0f)]
-    [SerializeField] private float towardsAnchorAlignmentFactor = 0.5f;
-    [Tooltip("1 when aligned, 0 when opposite")]
-    [Range(0.0f, 1.0f)]
-    [SerializeField] private float currentDirectionAlignmentFactor = 0.5f;
     
-    // private state 
-    private Vector3 _currentMoveDirection;
-    private float _changeDirectionTimer;
+    // state 
+    private enum MoveState { MoveToOrigin, MoveRandomly }
+    private MoveState _currentMoveState;
+    private Vector3 _anchorPosition;
+    private Vector2 _perlinSamplePoint; 
 
     // Start is called before the first frame update
     void Start() {
-        _currentState = State.MoveToOrigin;
+        _currentMoveState = MoveState.MoveToOrigin;
+        _perlinSamplePoint = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_currentState == State.MoveToOrigin) UpdateMoveToOrigin();
+        if (_currentMoveState == MoveState.MoveToOrigin) UpdateMoveToOrigin();
         else UpdateMoveRandomly();
     }
 
@@ -46,53 +41,88 @@ public class EnemyMovement : MonoBehaviour {
         transform.Translate(directionToOrigin * (Time.deltaTime * originMoveSpeed));
 
         if (distanceToOrigin < 1) {
-            _currentState = State.MoveRandomly;
+            _currentMoveState = MoveState.MoveRandomly;
             movedToOrigin.Raise();
         }
     }
 
     private void UpdateMoveRandomly() {
-        // maybe try having enemies accelerate forwards/backwards but move sharply left/right
-        // to look like theyre racing 
+        if (IsOffscreen()) {
+            _currentMoveState = MoveState.MoveToOrigin;
+            _perlinSamplePoint = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f));
+            return;
+        }
         
-        // pick a new direction
-        if (_changeDirectionTimer <= 0) {
-            ChooseNewRandomMoveDirection();
-            _changeDirectionTimer = changeDirectionTime;
-        }
-        // move in direction
-        else {
-            transform.Translate(_currentMoveDirection * (Time.deltaTime * moveSpeed));
-            _changeDirectionTimer -= Time.deltaTime;
-        }
-
+        PerlinMovement();
     }
 
-    private void ChooseNewRandomMoveDirection() {
-        Vector3 toAnchor = _anchorPosition - transform.position;
+    private void PerlinMovement() {
+        float perlinSampleSpeed = 0.5f;
+        float border = 0.2f;
         
-        Vector3 newDirection;
-        bool newDirectionAccepted = true;
-        do {
-            newDirection = Random.onUnitSphere;
-            
-            // if too far from anchor point, must be in direction of anchor point 
-            bool farFromAnchor = toAnchor != Vector3.zero && toAnchor.magnitude >= randomMoveRadius;
-            if (farFromAnchor || IsOffscreen()) {
-                var dot = Vector3.Dot(newDirection.normalized, toAnchor.normalized);
-                var f = (dot + 1) / 2.0f; // 1 when aligned, 0 when opposite 
-                newDirectionAccepted = f >= towardsAnchorAlignmentFactor;
-            }
-            
-            // must be similar to current direction
-            else if (_currentMoveDirection != Vector3.zero) {
-                var dot = Vector3.Dot(newDirection.normalized, _currentMoveDirection.normalized);
-                var f = (dot + 1) / 2.0f; // 1 when aligned, 0 when opposite 
-                newDirectionAccepted = f >= currentDirectionAlignmentFactor;
-            }
-        } while (!newDirectionAccepted);
-        
-        _currentMoveDirection = newDirection;
+        // get random perlin value for movement this frame 
+        Vector2 perlin = Vector2.zero;
+        perlin.x = Mathf.Clamp(Mathf.PerlinNoise1D(_perlinSamplePoint.x), 0, 1); // [0, 1]
+        perlin.y = Mathf.Clamp(Mathf.PerlinNoise1D(_perlinSamplePoint.y), 0, 1); // [0, 1]
+        _perlinSamplePoint += perlinSampleSpeed * Time.deltaTime * Vector2.one;
+
+        // set midpoint based on bounds & constrain perlin output
+        Vector3 offset = transform.position - _anchorPosition; // [-radius, +radius]
+        Vector2 t;
+        t.x = Mathf.InverseLerp(-randomMoveArea.x, randomMoveArea.x, offset.x);
+        t.y = Mathf.InverseLerp(-randomMoveArea.y, randomMoveArea.y, offset.y);
+        Vector2 midpoint = new Vector2(0.5f, 0.5f);
+        if (t.x <= border) {
+            var f = Mathf.InverseLerp(0f, border, t.x);
+            midpoint.x = Mathf.Lerp(0f, 0.5f, f);
+        }
+        else if (t.x >= 1 - border) {
+            var f = Mathf.InverseLerp(1f - border, 1f, t.x);
+            midpoint.x = Mathf.Lerp(0.5f, 1f, f);
+        }
+        if (t.y <= border) {
+            var f = Mathf.InverseLerp(0f, border, t.y);
+            midpoint.y = Mathf.Lerp(0f, 0.5f, f);
+        }
+        else if (t.y >= 1 - border) {
+            var f = Mathf.InverseLerp(1f - border, 1f, t.y);
+            midpoint.y = Mathf.Lerp(0.5f, 1f, f);
+        }
+
+        // constrain perlin output based on midpoint
+        Vector2 direction = Vector2.zero;
+        Vector2 magnitude = Vector2.zero; // [0, 1]
+        // X
+        if (perlin.x < midpoint.x) {
+            direction.x = -1;
+            magnitude.x = Mathf.InverseLerp(0, midpoint.x, midpoint.x - perlin.x);
+        }
+        else if (perlin.x > midpoint.x) {
+            direction.x = 1;
+            magnitude.x = Mathf.InverseLerp(midpoint.x, 1, perlin.x);
+        }
+        // Y
+        if (perlin.y < midpoint.y) {
+            direction.y = -1;
+            magnitude.y = Mathf.InverseLerp(0, midpoint.y, midpoint.y - perlin.y);
+        }
+        else if (perlin.y > midpoint.y) {
+            direction.y = 1;
+            magnitude.y = Mathf.InverseLerp(midpoint.y, 1, perlin.y);
+        }
+
+        // set speed based on magnitude
+        Vector2 speed = Vector2.zero;
+        if (magnitude.x != 0) {
+            speed.x = randomMoveSpeed.LerpValue(magnitude.x);
+        }
+        if (magnitude.y != 0) {
+            speed.y = randomMoveSpeed.LerpValue(magnitude.y);
+        }
+
+        // translate 
+        Vector3 velocity = new Vector3(direction.x * speed.x, direction.y * speed.y, 0);
+        transform.Translate(Time.deltaTime * velocity);
     }
 
     public void SetAnchor(Vector3 position) {
@@ -103,9 +133,11 @@ public class EnemyMovement : MonoBehaviour {
         var pos = transform.position;
         bool offscreen = pos.x < -onscreenSize.x || pos.x > onscreenSize.x || pos.y < -onscreenSize.y ||
                          pos.y > onscreenSize.y;
-        // if (offscreen) {
-        //     Debug.Log(name + " is offscreen");
-        // }
         return offscreen;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(_anchorPosition, new Vector3(2*randomMoveArea.x, 2*randomMoveArea.y, 1));
     }
 }
